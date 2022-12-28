5dbcdf7d578be3eed7000a1e9bd9209b7102fe28
Apokaliko <40949229+Apokaliko@users.noreply.github.com>
2022-12-28 11:21:11 +0300

    Add files via upload

---
+++ CalculateArea.cs
@@ -0,0 +1,63 @@
+﻿namespace Mindbox
+{
+    public abstract class Shape
+    {
+        public abstract double CalculateArea();
+    }
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public override double CalculateArea()
+        {
+            const double pi = 3.14;
+            return pi * radius * radius;
+        }
+    }
+
+    public class Triangle : Shape
+    {
+        private double a, b, c;
+
+        public Triangle(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public override double CalculateArea()
+        {
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+
+        public bool RightTriangle()
+        {
+            return a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a;
+        }
+    }
+    public static class ShapeFactory
+    {
+        public static Shape CreateShape(string shapeName, params double[] parameters)
+        {
+            switch (shapeName)
+            {
+                case "Circle":
+                    return new Circle(parameters[0]);
+                case "Triangle":
+                    return new Triangle(parameters[0], parameters[1], parameters[2]);
+                default:
+                    throw new ArgumentException("Неверное имя фигуры");
+            }
+        }
+    }
+
+
+}
+
---
+++ Test.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+namespace Mindbox.Tests
+{
+    public class AreaTests
+    {
+        [Test]
+        public void TestCircleArea()
+        {
+            Circle circle = new Circle(1.234);
+            double area = circle.CalculateArea();
+            Assert.AreEqual(4.78145384, area);
+        }
+
+        [Test]
+        public void TestTriangleArea()
+        {
+            Triangle triangle = new Triangle(2.0, 2.0, 3.0);
+            double area = triangle.CalculateArea();
+            Assert.AreEqual(1.984313483298443, area);
+        }
+
+        [Test]
+        public void TestTriangleIsRight()
+        {
+            Triangle triangle = new Triangle(6.0, 8.0, 10.0);
+            bool isRightTriangle = triangle.RightTriangle();
+            Assert.IsTrue(isRightTriangle);
+
+            Triangle triangle2 = new Triangle(7.0, 3.0, 4.0);
+            isRightTriangle = triangle2.RightTriangle();
+            Assert.IsFalse(isRightTriangle);
+        }
+    }
+}
